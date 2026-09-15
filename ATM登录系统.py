c924c6b8235c71fa81e8312594cf4bab919d32e0
duningning22-cmyk <duningning22-cmyk@users.noreply.github.com>
2026-09-15 10:45:40 +0200

    改回

--- ATM登录系统.py
+++ ATM登录系统.py
@@ -32,7 +32,7 @@ while True:
         else:
             balance = balance - int(remove)
     elif choice == '4':
-        print('Have an nice day!')
+        print('Have a nice day!')
         break
     else:
         print('enter a valid choice: ')
